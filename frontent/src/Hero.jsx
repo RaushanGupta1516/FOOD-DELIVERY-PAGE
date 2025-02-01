0ec64e2f574931f2952534991d8dd6b096a789c4
Raushan Gupta <raushangupta5597@gmail.com>
2025-02-01 23:03:00 +0530

    Update Hero.jsx

--- frontent/src/Hero.jsx
+++ frontent/src/Hero.jsx
@@ -2,10 +2,10 @@ import AnchorLink from "react-anchor-link-smooth-scroll";
 const Hero = () => {
 	return (
 		<div className="relative mx-4 sm:mx-6 md:ml-32 md:mr-28 ">
-			{/* Hero Image */}
+			
 			<img src="hero.png" alt="hero" className="w-full h-auto object-cover" />
 
-			{/* Hero Text */}
+	
 			<div className="absolute top-0 x4:top-4 x5:top-8  sm:top-6 lg:top-20 left-1/2 transform -translate-x-1/2    w-11/12  flex flex-col justify-center  sm:gap-4 x4:gap-1  text-white text-center ">
 				<h1 className="font-bold text-xl x4:text-2xl sm:text-4xl md:text-5xl lg:text-6xl leading-snug sm:leading-tight">
 					Order your <br /> favourite food here!
